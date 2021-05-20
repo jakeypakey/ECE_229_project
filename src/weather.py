77c37eb9b366ad9cb53d01d2c3cb72375aaef243
jake <jpollard@ucsd.edu>
2021-05-19 17:43:42 -0700

    weather

--- src/weather.py
+++ src/weather.py
@@ -1,6 +1,5 @@
 import requests as req
-
-API_KEY = 'd078ff41af2f9943ddea5b49d24e6c27'
+from pickle import load
 def getWeather(lat=32.8800806,lng=-117.237558):
     """fetches weather parameters for the provided latitude and longitude
     more info at https://openweathermap.org/current
@@ -14,8 +13,11 @@ def getWeather(lat=32.8800806,lng=-117.237558):
     assert isinstance(lat,float) and isinstance(lng,float)
     if lat > 90 or lat <-90 or lng > 180 or lng <-180:
         raise ValueError
+
 #api.openweathermap.org/data/2.5/weather?lat=33.568483&lon=-117.731516&appid=d078ff41af2f9943ddea5b49d24e6c27
     url = 'http://api.openweathermap.org/data/2.5/weather'
+    with open('../data/key.pkl','rb') as fi:
+        API_KEY = load(fi)['API_KEY']
     params = {'lat' : lat,
               'lon' : lng,
               'appid' : API_KEY}

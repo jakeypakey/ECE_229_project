3fcabcea150da8b65a773a6dc0ffdbb419a89ef5
RamDyuthi <47389001+RamDyuthi@users.noreply.github.com>
2021-06-02 14:30:17 -0700

    Add files via upload

--- site/screens/comparisonScreen.py
+++ site/screens/comparisonScreen.py
@@ -19,15 +19,33 @@ from plotly.subplots import make_subplots
 
 from app import app
 
-comparisons_df = pd.read_csv("../data/comparisons.csv")
+comparisons_df = pd.read_csv("comparisons_impute.csv")
+
+comparisons_df = comparisons_df.rename(columns={'Severity_1.0':'Severity_1',
+ 'Severity_2.0':'Severity_2',
+ 'Severity_3.0':'Severity_3',
+ 'Severity_4.0':'Severity_4'})
+
+comparisons_df_normalize = pd.read_csv("comparisons_impute_normalize.csv")
+comparisons_df_normalize = comparisons_df_normalize.rename(columns={'Severity_1.0':'Severity_1',
+ 'Severity_2.0':'Severity_2',
+ 'Severity_3.0':'Severity_3',
+ 'Severity_4.0':'Severity_4'})
 
 thres_category = {'Pressure':{'thresolds':[29.5,30.2],
                               'category':['Low','Moderate','High']},
-                  'TrafficStuckTime':{'thresolds':[35,50,70,90],
-                              'category':['VeryLow','Low','Moderate','High','VeryHigh']},
+                  'AccidentDuration':{'thresolds':[35,50,70,90],
+                              'category':['Very Low','Low','Moderate','High','Very High']},
                   'Temperature':{'thresolds':[0,30,60,80,100],
-                              'category':['VeryVeryLow','VeryLow','Low','Moderate','High','VeryHigh']}
-                  }
+                              'category':['VeryVeryLow','VeryLow','Low','Moderate','High','VeryHigh']},
+                  'WindSpeed':{'thresolds':[8,13,19,25],
+                              'category':['Calm','GentleBreeze','ModerateBreeze','FreshBreeze','StrongBreeze']},
+                  'Humidity':{'thresolds':[30,50],
+                              'category':['Low','Moderate','High']},
+                  'TimeOfDay':{'thresolds':[6,12,18],
+                              'category':['Night','Day','Noon','Night']},
+                 }
+thres_category['TimeOfDay']['category'] = ['Day','Noon','Night']
 
 thres_category['DayOfWeek'] = {'thresolds':[],
                                'category':["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]}
@@ -47,6 +65,31 @@ for feature in features:
     else:
         columns_mapping[feature] = [feature+'_'+value for value in thres_category[feature]['category']]
         
+# print(features_dict)
+
+
+
+
+legend_names = {}
+for feature in columns_mapping:
+    
+    if feature not in thres_category or feature=="DayOfWeek":
+        for col in columns_mapping[feature]:
+            legend_names[col] = col.replace(feature+"_","")
+    else:
+        if feature=="TimeOfDay":
+            legend_names["TimeOfDay_Day"] = "Day (6:00-11:59)"
+            legend_names["TimeOfDay_Noon"] = "Noon (12:00-17:59)"
+            legend_names["TimeOfDay_Night"] = "Night (18:00-5:59)"
+            
+        else:
+            for idx,col in enumerate(columns_mapping[feature]):
+                if idx==0:
+                    legend_names[col] = col.replace(feature+"_","")+" (<"+str(thres_category[feature]['thresolds'][0])+")"
+                elif idx==len(feature)-1:
+                    legend_names[col] = col.replace(feature+"_","")+" (>"+str(thres_category[feature]['thresolds'][-1])+")"
+                else:
+                    legend_names[col] = col.replace(feature+"_","")+" ("+str(thres_category[feature]['thresolds'][idx-1])+"-"+str(thres_category[feature]['thresolds'][idx-1])+")"
 
 
 years = comparisons_df['Year'].unique()
@@ -59,93 +102,131 @@ States_names.insert(0,'All')
 states_dict = [{'label': state, 'value': state} for state in States_names]
 
 
-layout = html.Div(className='', children= 
-    [
-        html.Div(className='row flex-display', children=[
-            html.Div(className="pretty_container twelve columns", children= 
-            [
-                dcc.Graph(id="graph-comparison",style={"height": 500}),
-            ]),
-        ]),
-        html.Div(className='row flex-display', children=
-        [
-            html.Div(className='pretty_container twelve columns', children=
-                [
-                    # html.H2("Comparisons"),
-                    html.Div(className='two columns', children=
-                        [
-                            html.Div(
-                                [
-                                    html.H3('States'),
-                                    dcc.Dropdown(
-                                        options=states_dict,
-                                        multi=False,
-                                        value="All",
-                                        id="states-comparison"
-                                    )
-                                ]
-                            ),
-                            html.Div(
-                                [
-                                    html.H3('Feature 1'),
-                                    dcc.Dropdown(
-                                        options=features_dict,
-                                        multi=False,
-                                        value="Severity",
-                                        id="feature1"
-                                    ),
-                                ]
-                            ),
-                            html.Div(
-                                [
-                                    html.H3('Feature 2'),
-                                    dcc.Dropdown(
-                                        options=features_dict,
-                                        multi=False,
-                                        value="TrafficStuckTime",
-                                        id="feature2"
-                                    )
-                                ]
-                            )
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets) 
 
-                        ]
-                    ),
-                    html.Div(className='ten columns', children=
-                        [
-                            dcc.Slider(
-                                min=2016,
-                                max=2019,
-                                step=1,
-                                marks={
-                                    2016: '2016',
-                                    2017: '2017',
-                                    2018: '2018',
-                                    2019: '2019'
-                                },
-                                value=2016,
-                                id='Year-comparison')
-                        ],
-                        # style={'width': '70%' } 
-                    )
-            ])
-    ]),
+layout = html.Div([
+    html.H2("Comparisons"),
+
+
+    # ], className="container"),
+    
+    html.Div([
+        # html.Div([
+        #     html.H3('States'),
+        #     dcc.Dropdown(
+        #         options=states_dict,
+        #         multi=False,
+        #         value="All",
+        #         id="states"
+        #     )  
+        # ], className="six columns"),
+        
+        html.Div([
+            # html.H3('Normalization'),
+            html.Div([
+                html.H3('States'),
+                dcc.Dropdown(
+                    options=states_dict,
+                    multi=False,
+                    value="All",
+                    id="states"
+                ) , ]),
+            html.Div([
+                html.H3('y-axis'),
+                dcc.RadioItems(
+                options=[
+                    {'label': '#Accidents', 'value': '#Accidents'},
+                    {'label': 'percentage', 'value': 'percentage'}
+                ],
+                value='#Accidents',
+                id='normalization'
+                )  
+                ]) ]),
+        
+        # html.Div([
+        #     html.H3('Date'),
+        #     dcc.RangeSlider(
+        #                     id='rangeslider',
+        #                     min=0,
+        #                     max=accidents['year_month'].nunique()-1,
+        #                     value=[0, accidents['year_month'].nunique()-1],
+        #                     allowCross=False
+        #                     )
+        # ], className="six columns"),
+        
+        html.Div([
+            # html.H3('Normalization'),
+            html.Div([
+                html.H3('Feature 1'),
+                dcc.Dropdown(
+                    options=features_dict,
+                    multi=False,
+                    value="Severity",
+                    id="feature1"
+                ) , ]),
+            html.Div([
+                html.H3('Feature 2'),
+                dcc.Dropdown(
+                    options=features_dict,
+                    multi=False,
+                    value="AccidentDuration",
+                    id="feature2"
+                )  ])
+            
+            # dcc.RadioItems(
+            #     options=[
+            #         {'label': 'without Normalization', 'value': 'without Normalization'},
+            #         {'label': 'per million people', 'value': 'per million people'}
+            #     ],
+            #     value='per million people',
+            #     id='normalization'
+            #     )  
+        ],className="six columns"),
+        
+        
+        
+        
+    ], className="container"),
+
+    
+    
+    dcc.Graph(id="graph",style={"height": 500}),
+    
+
+    html.Div(children=[dcc.Slider(
+        min=2016,
+        max=2019,
+        step=1,
+        marks={
+            2016: '2016',
+            2017: '2017',
+            2018: '2018',
+            2019: '2019'
+        },
+        value=2016,
+        id='Year')],style={'width': '70%', 'padding-left': '15%','padding-right': '15%' } 
+        )
     
 ])
 
-@app.callback(Output("graph-comparison", "figure"), 
-                    [
-                        Input('states-comparison', 'value'),
-                        Input('Year-comparison','value'),
+# 'width':800,'margin-left': '5px' 'display': 'flex', , 'justify-content': 'center' 'align-items': 'center'
+@app.callback(Output("graph", "figure"), 
+                       [Input('states', 'value'),
+                        Input('Year','value'),
                         Input('feature1','value'),
-                        Input('feature2','value')
-                    ]
-                )
-def update_graphs(State,year,feature1,feature2):
+                        Input('feature2','value'),
+                        Input('normalization','value')])
+def update_graphs(State,year,feature1,feature2,normalization):
     
     if not State:
         State = "All"
-     
-    df = comparisons_df[comparisons_df['State']==State]
+    
+    if normalization=='#Accidents': 
+        df = comparisons_df[comparisons_df['State']==State]
+        
+    elif normalization=='percentage':
+        df = comparisons_df_normalize[comparisons_df['State']==State]
     
     fig = make_subplots(rows=2, cols=1, subplot_titles = (feature1, feature2))
     
@@ -155,7 +236,7 @@ def update_graphs(State,year,feature1,feature2):
             fig.add_trace(go.Bar(
                 y=df[df['Year']==year][column],
                 x=list(range(1,13)),
-                name=column.replace(feature+"_",""),
+                name=column.replace(feature+"_",""), #legend_names[column],#
                 legendgroup = str(i+1)
                 ),row=i+1, col=1)
 
@@ -171,10 +252,11 @@ def update_graphs(State,year,feature1,feature2):
                     ticktext = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June','July','Aug','Sep','Oct','Nov','Dec']
                     ),
                       legend_tracegroupgap = 100,)
-    
+    max_range = df[columns_mapping['TimeOfDay']].sum(axis=1).max()
+    fig.update_yaxes(title_text=normalization,range=[0,max_range])
 
     return fig 
 
 
 
-
+# app.run_server(d
--- site/screens/statesScreen.py
+++ site/screens/statesScreen.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun  1 20:04:18 2021
+Created on Sun May 30 22:28:14 2021
 
 @author: srist
 """
@@ -15,8 +15,7 @@ from dash.dependencies import Input, Output
 
 from app import app
 
-# load dataset
-accidents = pd.read_csv("data/accidents_visualization.csv")
+accidents = pd.read_csv("accidents_visualization.csv")
 
 num_frames = len(accidents['year_month'].unique())
 fig = px.choropleth(accidents,               
@@ -37,49 +36,42 @@ States_names.sort()
 States_names.insert(0,'All')
 states_dict = [{'label': state, 'value': state} for state in States_names]
 
+layout = html.Div([
+    html.H2("Number of accidents for each state"),
+    html.Div([
+        html.Div([
+            html.H3('States'),
+            dcc.Dropdown(
+                options=states_dict,
+                multi=True,
+                value=["All"],
+                id="states"
+            )  
+        ], className="six columns"),
 
-layout = html.Div(id='states-screen', className='row flex-display', children=
-    [
-        html.Div(className="pretty_container four columns", children=
-        [
-            html.H4("Number of accidents for each state"),
-            html.Div(className='input-field', children=
-            [
-                html.Div([
-                    html.H5('States'),
-                    dcc.Dropdown(
-                        options=states_dict,
-                        multi=True,
-                        value=["All"],
-                        id="states"
-                    )  
-                ]),
-            ]),
-            html.Div( className='input-field', children=
-            [
-                html.H5('Normalization'),
-                dcc.RadioItems(
-                    options=[
-                        {'label': 'without Normalization', 'value': 'without Normalization'},
-                        {'label': 'per million people', 'value': 'per million people'}
-                    ],
-                    value='per million people',
-                    id='normalization'
-                    )  
-            ]),
-        ]),
-        html.Div(className="pretty_container eight columns", children=
-        [
-            dcc.Graph(id="graph",style={"height": 500}),
-        ])
-    ]
 
-    # html.Div(className='nav-button-next', children=
-    #     [
-    #         html.A('Next', href='#conditions-screen')
-    #     ]
-    # )
-)
+        html.Div([
+            html.H3('Normalization'),
+            dcc.RadioItems(
+                options=[
+                    {'label': '#Accidents', 'value': 'without Normalization'},
+                    {'label': '#Accident per million', 'value': 'per million people'}
+                ],
+                value='per million people',
+                id='normalization'
+                )  
+        ], className="six columns"),
+        
+        
+        
+        
+    ], className="container"),
+    
+    dcc.Graph(id="graph",style={"height": 500})
+    
+])
+
+
 
 
 @app.callback(Output("graph", "figure"), 
@@ -89,18 +81,20 @@ def update_graphs(states,normalization):
     
     if not states:
         states = ["All"]
+        
+
 
     if 'All' in states:
         
         df = accidents
     else:
         df = accidents[accidents['State'].isin(states)]
-        
     if normalization=='without Normalization':
         color_column  = "counts"
         color_range = (0, 10000)
     elif normalization=='per million people':
         color_column  = "counts per million"
+        
         if 'All' in states:
             color_range = (0, 60)
         else:
@@ -125,6 +119,7 @@ def update_graphs(states,normalization):
                         # category_orders = {'frames':list(range(1,num_frames+1))},
                         )
     fig.update_layout(geo_scope="usa")
-
-    return fig 
     
+
+    
+    return fig 

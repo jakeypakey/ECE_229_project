419df57867beab36e8641873e287fbdea385bf70
RamDyuthi <47389001+RamDyuthi@users.noreply.github.com>
2021-06-02 07:47:08 -0700

    Add files via upload

--- site/screens/comparisonScreen.py
+++ site/screens/comparisonScreen.py
@@ -6,7 +6,6 @@ Created on Tue Jun  1 22:55:23 2021
 """
 
 
-
 import numpy as np
 import pandas as pd
 import plotly.express as px
@@ -19,17 +18,32 @@ from plotly.subplots import make_subplots
 
 from app import app
 
-# load dataset
-comparisons_df = pd.read_csv("data/comparisons.csv")
+comparisons_df = pd.read_csv("comparisons.csv")
+
+thres_category = {'Pressure':{'thresolds':[29.5,30.2],
+                              'category':['Low','Moderate','High']},
+                  'TrafficStuckTime':{'thresolds':[35,50,70,90],
+                              'category':['VeryLow','Low','Moderate','High','VeryHigh']},
+                  'Temperature':{'thresolds':[0,30,60,80,100],
+                              'category':['VeryVeryLow','VeryLow','Low','Moderate','High','VeryHigh']},}
+
+
 
 columns = list(comparisons_df.keys())
 features = list(comparisons_df.keys())
 for column in ['State', 'year_month','Month', 'Year']:
     features.remove(column)
 features = np.unique([column.split("_")[0] for column in features] ) 
+features_dict = [{'label': feature, 'value': feature} for feature in features]
+
 columns_mapping = {}
 for feature in features:
-    columns_mapping[feature] = [column for column in columns if feature in column]
+    if feature not in thres_category.keys():
+        columns_mapping[feature] = [column for column in columns if feature in column]
+    else:
+        columns_mapping[feature] = [feature+'_'+value for value in thres_category[feature]['category']]
+        
+
 
 years = comparisons_df['Year'].unique()
 years.sort()
@@ -40,9 +54,6 @@ States_names.sort()
 States_names.insert(0,'All')
 states_dict = [{'label': state, 'value': state} for state in States_names]
 
-
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets) 
 
 layout = html.Div([
     html.H2("Comparisons"),
@@ -57,7 +68,31 @@ layout = html.Div([
             )  
         ], className="six columns"),
 
+        html.Div([
+            # html.H3('Normalization'),
+            html.Div([
+                html.H3('Feature 1'),
+                dcc.Dropdown(
+                    options=features_dict,
+                    multi=False,
+                    value="Severity",
+                    id="feature1"
+                ) , ]),
+            html.Div([
+                html.H3('Feature 2'),
+                dcc.Dropdown(
+                    options=features_dict,
+                    multi=False,
+                    value="TrafficStuckTime",
+                    id="feature2"
+                )  ])
+            
 
+        ],className="six columns"),
+        
+        
+        
+        
     ], className="container"),
 
     
@@ -81,20 +116,24 @@ layout = html.Div([
     
 ])
 
+# 'width':800,'margin-left': '5px' 'display': 'flex', , 'justify-content': 'center' 'align-items': 'center'
 @app.callback(Output("graph", "figure"), 
                        [Input('states', 'value'),
-                        Input('Year','value')])
-def update_graphs(State,year):
+                        Input('Year','value'),
+                        Input('feature1','value'),
+                        Input('feature2','value')])
+def update_graphs(State,year,feature1,feature2):
     
     if not State:
         State = "All"
      
     df = comparisons_df[comparisons_df['State']==State]
     
-    fig = make_subplots(rows=2, cols=1, subplot_titles = (features[0], features[1]))
-
-    for i in [0,1]:
-        for column in columns_mapping[features[i]]:
+    fig = make_subplots(rows=2, cols=1, subplot_titles = (feature1, feature2))
+    
+    
+    for i,feature in enumerate([feature1,feature2]):
+        for column in columns_mapping[feature]:
             fig.add_trace(go.Bar(
                 y=df[df['Year']==year][column],
                 x=list(range(1,13)),
@@ -114,9 +153,11 @@ def update_graphs(State,year):
                     ticktext = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June','July','Aug','Sep','Oct','Nov','Dec']
                     ),
                       legend_tracegroupgap = 100,)
-
+    
+    
+    fig.update_layout(color_discrete_sequence=px.colors.qualitative.G10)
+    
     return fig 
-
 
 
 
